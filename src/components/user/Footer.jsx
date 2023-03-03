cd77a351cd1af4bee94d0aa51958383905aba873
aungchanmyaethaw <aungchanmyaethaw0610@gmail.com>
2023-03-03 21:13:53 +0630

    user responsive

--- src/components/user/Footer.jsx
+++ src/components/user/Footer.jsx
@@ -13,124 +13,132 @@ const Footer = () => {
   return (
     <footer className="bg-dreamLabColor3 bg-opacity-10 font-poppins pt-8 py-4">
       <div className="pb-12 max-w-[1440px] mx-auto">
-        <div className="grid items-baseline content-center sm:grid-cols-2 xl:grid-cols-4">
+        <div className="flex justify-between flex-col lg:flex-row">
           {/* col-1 */}
-          <div className="p-4">
-            <img src={FooterLogo} alt="logo" className="object-contain" />
-          </div>
+
+          <img
+            src={FooterLogo}
+            alt="logo"
+            className="mx-auto lg:ml-0 lg:mr-0 mb-4 lg:mb-0"
+          />
+
           {/* col-2 */}
-          <div className="p-4 ">
-            <h2 className="mb-6 text-2xl font-semibold  text-dreamLabColor4">
-              Get Started
-            </h2>
-            <ul className="flex flex-col gap-6">
-              <Link
-                href="/"
-                className="text-lg font-medium text-dreamLabColor4  w-max"
-              >
-                Books
-              </Link>
-              <Link
-                href="/"
-                className="text-lg font-medium text-dreamLabColor4  w-max"
-              >
-                Articles
-              </Link>
-              <Link
-                href="/"
-                className="text-lg font-medium text-dreamLabColor4  w-max"
-              >
-                Podcasts
-              </Link>
-              <Link
-                href="/"
-                className="text-lg font-medium  text-dreamLabColor4 w-max"
-              >
-                Blogs
-              </Link>
-            </ul>
-          </div>
-          {/* col-3 */}
-          <div className="p-4">
-            <h2 className="mb-6 text-2xl font-semibold  text-dreamLabColor4">
-              Useful Links
-            </h2>
-            <ul className="flex flex-col gap-6">
-              <Link
-                href="/"
-                className="text-lg font-medium text-dreamLabColor4  w-max  "
-              >
-                Library
-              </Link>
-              <Link
-                href="/"
-                className="text-lg font-medium text-dreamLabColor4  w-max  "
-              >
-                Why Choose
-              </Link>
-              <Link
-                href="/"
-                className="text-lg font-medium text-dreamLabColor4  w-max  "
-              >
-                Pricing
-              </Link>
-              <Link
-                href="/"
-                className="text-lg font-medium text-dreamLabColor4  w-max  "
-              >
-                About Us
-              </Link>
-            </ul>
-          </div>
-          {/* col-4 */}
-          <div className="p-4">
-            <h2 className="mb-6 text-2xl font-semibold   text-dreamLabColor4">
-              Contact Us
-            </h2>
-            <ul className="mb-12">
-              <li className="flex gap-4 items-center mb-6">
-                <div className="w-[34px] h-[34px] flex items-center justify-center rounded-full bg-dreamLabColor1">
-                  <MdPhoneInTalk className="fill-white" size={18} />
-                </div>
-                <span className="text-dreamLabColor4 font-semibold text-lg">
-                  09794461888
-                </span>
-              </li>
-              <li className="flex gap-4 items-center ">
-                <div className="w-[34px] h-[34px] flex items-center justify-center rounded-full bg-dreamLabColor1">
-                  <HiEnvelope className="fill-white" size={18} />
-                </div>
-                <span className="text-dreamLabColor4 font-semibold text-lg">
-                  www.m.me/dreamlab.news
-                </span>
-              </li>
-            </ul>
-            <div className="flex">
-              <h3 className="text-dreamLabColor4 font-semibold text-2xl mr-4">
-                Follow Us
-              </h3>
-              <ul className="flex gap-4 items-center">
-                <Link to="/">
-                  <AiFillFacebook className="text-dreamLabColor1 text-3xl" />
+          <div className="flex  lg:justify-evenly grow ">
+            <div className="p-4 ">
+              <h2 className="mb-6 text-base lg:text-2xl font-semibold  text-dreamLabColor4">
+                Get Started
+              </h2>
+              <ul className="flex flex-col gap-6">
+                <Link
+                  href="/"
+                  className="lg:text-lg text-xs font-medium text-dreamLabColor4  w-max"
+                >
+                  Books
                 </Link>
-                <Link to="/">
-                  <AiFillInstagram className="text-dreamLabColor1 text-3xl" />
+                <Link
+                  href="/"
+                  className="lg:text-lg text-xs font-medium text-dreamLabColor4  w-max"
+                >
+                  Articles
                 </Link>
-                <Link to="/">
-                  <AiFillLinkedin className="text-dreamLabColor1 text-3xl" />
+                <Link
+                  href="/"
+                  className="lg:text-lg text-xs font-medium text-dreamLabColor4  w-max"
+                >
+                  Podcasts
                 </Link>
-                <Link to="/">
-                  <AiFillYoutube className="text-dreamLabColor1 text-3xl" />
+                <Link
+                  href="/"
+                  className="lg:text-lg text-xs font-medium  text-dreamLabColor4 w-max"
+                >
+                  Blogs
                 </Link>
               </ul>
             </div>
+
+            <div className="p-4 ">
+              <h2 className="mb-6 text-base lg:text-2xl font-semibold  text-dreamLabColor4 ">
+                Useful Links
+              </h2>
+              <ul className="flex flex-col gap-6">
+                <Link
+                  href="/"
+                  className="lg:text-lg text-xs font-medium text-dreamLabColor4  w-max  "
+                >
+                  Library
+                </Link>
+                <Link
+                  href="/"
+                  className="lg:text-lg text-xs font-medium text-dreamLabColor4  w-max  "
+                >
+                  Why Choose
+                </Link>
+                <Link
+                  href="/"
+                  className="lg:text-lg text-xs font-medium text-dreamLabColor4  w-max  "
+                >
+                  Pricing
+                </Link>
+                <Link
+                  href="/"
+                  className="lg:text-lg text-xs font-medium text-dreamLabColor4  w-max  "
+                >
+                  About Us
+                </Link>
+              </ul>
+            </div>
+
+            <div className="p-4 ">
+              <h2 className="mb-6 text-base lg:text-2xl font-semibold   text-dreamLabColor4 ">
+                Contact Us
+              </h2>
+              <ul className="mb-12">
+                <li className="flex gap-4 items-center mb-6">
+                  <div className="w-[20px] h-[20px] lg:w-[34px] lg:h-[34px] flex items-center justify-center rounded-full bg-dreamLabColor1">
+                    <MdPhoneInTalk className="fill-white text-xs lg:text-lg" />
+                  </div>
+                  <span className="text-dreamLabColor4 font-semibold lg:text-lg text-xs">
+                    09794461888
+                  </span>
+                </li>
+                <li className="flex gap-4 items-center ">
+                  <div className="w-[20px] h-[20px] lg:w-[34px] lg:h-[34px] flex items-center justify-center rounded-full bg-dreamLabColor1">
+                    <HiEnvelope className="fill-white text-xs lg:text-lg" />
+                  </div>
+                  <span className="text-dreamLabColor4 font-semibold lg:text-lg text-xs">
+                    www.m.me/
+                    <br className="lg:hidden block" />
+                    dreamlab.news
+                  </span>
+                </li>
+              </ul>
+              <div className="flex flex-col lg:flex-row ">
+                <h3 className="text-dreamLabColor4 font-semibold text-base lg:text-2xl mr-4 mb-4 lg:mb-0">
+                  Follow Us
+                </h3>
+                <ul className="flex gap-4 items-center">
+                  <Link to="/">
+                    <AiFillFacebook className="text-dreamLabColor1 lg:text-3xl text-2xl" />
+                  </Link>
+                  <Link to="/">
+                    <AiFillInstagram className="text-dreamLabColor1 lg:text-3xl text-2xl" />
+                  </Link>
+                  <Link to="/">
+                    <AiFillLinkedin className="text-dreamLabColor1 lg:text-3xl text-2xl" />
+                  </Link>
+                  <Link to="/">
+                    <AiFillYoutube className="text-dreamLabColor1 lg:text-3xl text-2xl" />
+                  </Link>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="mt-12">
+        {/* <div className="mt-12">
           <p className="text-textColor1">
             Copyright 2022 Dreamlabnews.All Rights reserved.
           </p>
-        </div>
+        </div> */}
       </div>
     </footer>
   );
