352128c8a7a863fef1c38b99d4653aa4f14d0017
Wendy <111409717+wendy-aik-maung@users.noreply.github.com>
2023-03-05 09:03:01 +0630

    init subscription

--- src/pages/admin/subscription/SubscriptionItem.jsx
+++ src/pages/admin/subscription/SubscriptionItem.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const SubscriptionItem = () => {
-  return <div>SubscriptionItem</div>;
+	return (
+		<div className="container mx-auto border-2 shadow-lg  p-10 my-6">
+			<h2>6Months Plan</h2>
+			<section className="mt-3 flex">
+				<p className="col-span-8">
+					Lorem ipsum dolor sit amet consectetur. Rutrum porttitor sagittis
+					ipsum consectetur cras. Varius nibh molestie fames integer leo proin
+					netus vulputate nunc.
+				</p>
+
+				<div className="col-span-4 ml-5">
+					<button className="btn text-dreamLabColor3">Edit</button>
+					<button className="ml-4  text-red-600">Delete</button>
+				</div>
+			</section>
+			<div className="mt-5 flex gap-x-5 ">
+				<p className="line-through">90,000 Ks</p>
+				<p className="text-dreamLabColor1 text-2xl font-semibold">49,000 Ks</p>
+			</div>
+		</div>
+	);
 };
 
 export default SubscriptionItem;
+// const styles = {
+// 	subscriptionContainer: css`
+// 		border: 1px solid red;
+// 	`,
+// };
--- src/pages/admin/subscription/index.jsx
+++ src/pages/admin/subscription/index.jsx
@@ -1,7 +1,33 @@
 import React from "react";
-
+import { useState } from "react";
+import { AiFillPlusCircle } from "react-icons/ai";
+import EmptyView from "../../../assets/EmptyView";
+import SubscriptionItem from "./SubscriptionItem";
 const SubscriptionIndex = () => {
-  return <div>SubscriptionIndex</div>;
+	const [subscription, setSubscription] = useState([1, 2]);
+
+	return (
+		<div className="container mx-auto">
+			<header className="flex justify-between">
+				<h3 className="font-semibold text-xl ">Subscription Plan Lists</h3>
+				<div className="btn-dreamLabColor2 flex items-center py-2 px-10 gap-x-2 font-normal text-lg ">
+					<AiFillPlusCircle /> Create Subscription
+				</div>
+			</header>
+			{subscription.length === 0 ? (
+				<section className="mt-48 text-center">
+					<div className="flex justify-center">
+						<EmptyView />
+					</div>
+					<p className="mt-5">You have no plans created yet</p>
+				</section>
+			) : (
+				<div>
+					<SubscriptionItem />
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default SubscriptionIndex;
