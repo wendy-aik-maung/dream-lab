9d820057d241cf7dd343506a89a81a768186f859
SiThuHtin <sithuhtin2022@gmail.com>
2023-03-06 00:14:05 +0700

    AdminsidebarLink,Header,sidebar,AdminLayout
    AdminsidebarLink,Header,sidebar,AdminLayout

--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -1,7 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import AdminSidebarLink from "./AdminSidebarLink";
+import { AiFillHome } from "react-icons/ai";
+import { ImBooks } from "react-icons/im";
+import { MdEventNote } from "react-icons/md";
+import { RiVipCrownFill } from "react-icons/ri";
+import { IoIosArrowDropdown } from "react-icons/io";
+import { AiFillFilePdf } from "react-icons/ai";
+import { FaUserAlt } from "react-icons/fa";
+import { MdArticle } from "react-icons/md";
+import { MdPodcasts } from "react-icons/md";
+import { FaYoutube } from "react-icons/fa";
+import { BiCategory } from "react-icons/bi";
+import { FaUsers } from "react-icons/fa";
+import { BiImage } from "react-icons/bi";
+import { AiFillCreditCard } from "react-icons/ai";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const [manage, setManage] = useState(false);
+  const [subscription, setSubscription] = useState(false);
+
+  return (
+    <div className="h-screen flex flex-col w-80 items-center py-8 bg-dreamLabColor4 text-white text-opacity-50">
+      <div className="flex items-center mb-8">
+        <span className="text-xl font-bold text-textColor3">
+          Admin Dashboard
+        </span>
+      </div>
+      <div className="flex flex-col">
+        <AdminSidebarLink to="/" icon={<AiFillHome />} title="Dashboard" />
+        <AdminSidebarLink to="/books" icon={<ImBooks />} title="Books" />
+        <AdminSidebarLink to="/plans" icon={<MdEventNote />} title="Plans" />
+        <AdminSidebarLink
+          to="/product"
+          icon={<RiVipCrownFill />}
+          title="Subscription plan"
+        />
+        <hr />
+        <div
+          className="flex justify-between items-center text-textColor3 py-4 text-xl cursor-pointer "
+          onClick={() => setManage(!manage)}
+        >
+          <h1>Manage</h1>
+          <IoIosArrowDropdown />
+        </div>
+        {manage && (
+          <div>
+            <AdminSidebarLink to="/books" icon={<ImBooks />} title="Books" />
+            <AdminSidebarLink
+              to="/ebooks"
+              icon={<AiFillFilePdf />}
+              title="E-Books"
+            />
+            <AdminSidebarLink
+              to="/articles"
+              icon={<MdArticle />}
+              title="Articles"
+            />
+            <AdminSidebarLink
+              to="/podcasts"
+              icon={<MdPodcasts />}
+              title="Podcasts"
+            />
+            <AdminSidebarLink
+              to="/videos"
+              icon={<FaYoutube />}
+              title="Videos"
+            />
+            <AdminSidebarLink
+              to="/plans"
+              icon={<MdEventNote />}
+              title="Plans"
+            />
+            <AdminSidebarLink
+              to="/category"
+              icon={<BiCategory />}
+              title="Category"
+            />
+            <AdminSidebarLink to="/users" icon={<FaUsers />} title="Users" />
+            <AdminSidebarLink to="/banner" icon={<BiImage />} title="Banner" />
+            <AdminSidebarLink
+              to="/"
+              icon={<AiFillCreditCard />}
+              title="Payment Method"
+            />
+          </div>
+        )}
+        <hr />
+
+        <div
+          className="flex justify-between items-center  text-textColor3 py-4 text-xl cursor-pointer"
+          onClick={() => setSubscription(!subscription)}
+        >
+          <h1>Subscription</h1>
+          <IoIosArrowDropdown />
+        </div>
+        {subscription && (
+          <div>
+            <AdminSidebarLink to="/" icon={<FaUserAlt />} title="Subscriber" />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
