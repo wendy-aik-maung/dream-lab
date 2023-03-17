e7a799243a82d241c6f51027e5b974c744417063
Wendy <111409717+wendy-aik-maung@users.noreply.github.com>
2023-03-17 20:42:17 +0630

    create category api feching

--- src/services/api/categoryApi.js
+++ src/services/api/categoryApi.js
@@ -3,11 +3,60 @@ import { getToken } from "../../utils/getToken";
 
 // ========== Get all category ========== //
 
-export const fetchCategories = async () => {};
+export const fetchCategories = async () => {
+	const token = getToken();
+	const requestOption = {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+		mode: "cors",
+		method: "GET",
+	};
+	try {
+		const response = await fetch(`${BASE_URL}categories`, requestOption);
+		const data = await response.json();
+
+		if (!response.ok) {
+			throw new Error(data.message);
+		}
+
+		return data;
+	} catch (error) {
+		throw error;
+	}
+};
 
 // ========== Create category ========== //
 
-export const createCategory = async (data) => {};
+export const createCategory = async (data) => {
+	const token = getToken();
+	const formData = new FormData();
+
+	formData.append("icon", image);
+	formData.append("name", string);
+	console.log(formData);
+
+	const requestOptions = {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+		mode: "cors",
+		method: "POST",
+		redirect: "follow",
+		body: formData,
+	};
+
+	try {
+		const response = await fetch(`${BASE_URL}categories`, requestOptions);
+		const data = await response.json();
+
+		if (!response.ok) throw new Error(data.message);
+
+		return data;
+	} catch (error) {
+		throw error;
+	}
+};
 
 // ========== Update  category ========== //
 
