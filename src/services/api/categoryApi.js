b1d742f0f03b33f75828adb14b2a04a7ee4e489c
aungchanmyaethaw <aungchanmyaethaw0610@gmail.com>
2023-03-18 19:20:23 +0630

    edit category fixed

--- src/services/api/categoryApi.js
+++ src/services/api/categoryApi.js
@@ -4,138 +4,139 @@ import { getToken } from "../../utils/getToken";
 // ========== Get all category ========== //
 
 export const fetchCategories = async () => {
-	const token = getToken();
-	const requestOption = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-		mode: "cors",
-		method: "GET",
-	};
-	try {
-		const response = await fetch(`${BASE_URL}categories`, requestOption);
-		const data = await response.json();
+  const token = getToken();
+  const requestOption = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    mode: "cors",
+    method: "GET",
+  };
+  try {
+    const response = await fetch(`${BASE_URL}categories`, requestOption);
+    const data = await response.json();
 
-		if (!response.ok) {
-			throw new Error(data.message);
-		}
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
 
-		return data;
-	} catch (error) {
-		throw error;
-	}
+    return data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 // ========== Get Single category ==========//
 
 export const fetchCategory = async (id) => {
-	var requestOptions = {
-		mode: "cors",
-		method: "GET",
-		redirect: "follow",
-	};
-	try {
-		const response = await fetch(`${BASE_URL}categories/${id}`, requestOptions);
-		const data = await response.json();
-		if (!response.ok) throw new Error(data.message);
+  var requestOptions = {
+    mode: "cors",
+    method: "GET",
+    redirect: "follow",
+  };
+  try {
+    const response = await fetch(`${BASE_URL}categories/${id}`, requestOptions);
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.message);
 
-		return data;
-	} catch (error) {
-		throw error;
-	}
+    return data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 // ========== Create category ========== //
 
 export const createCategory = async (data) => {
-	const { name, icon } = data;
+  const { name, icon } = data;
 
-	const token = getToken();
-	const formData = new FormData();
+  const token = getToken();
+  const formData = new FormData();
 
-	formData.append("icon", icon[0]);
-	formData.append("name", name);
+  formData.append("icon", icon[0]);
+  formData.append("name", name);
 
-	const requestOptions = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-		mode: "cors",
-		method: "POST",
-		redirect: "follow",
-		body: formData,
-	};
+  const requestOptions = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    mode: "cors",
+    method: "POST",
+    redirect: "follow",
+    body: formData,
+  };
 
-	try {
-		const response = await fetch(`${BASE_URL}categories`, requestOptions);
-		const data = await response.json();
+  try {
+    const response = await fetch(`${BASE_URL}categories`, requestOptions);
+    const data = await response.json();
 
-		if (!response.ok) throw new Error(data.message);
+    if (!response.ok) throw new Error(data.message);
 
-		return data;
-	} catch (error) {
-		throw error;
-	}
+    return data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 // ========== Update  category ========== //
 
-export const updateCategory = async (data) => {
-	const token = getToken();
-	const formData = new FormData();
+export const updateCategory = async (params) => {
+  const { data, id } = params;
 
-	formData.append("icon", data?.icon[0]);
-	formData.append("name", data?.name);
+  const token = getToken();
+  const formData = new FormData();
 
-	const requestOptions = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-		mode: "cors",
-		method: "PATCH",
-		redirect: "follow",
-		body: formData,
-	};
+  const icon = typeof data?.icon === "string" ? data.icon : data.icon?.[0];
 
-	try {
-		const response = await fetch(
-			`${BASE_URL}categories/${data?.id}`,
-			requestOptions
-		);
-		const responseData = await response.json();
+  formData.append("icon", icon);
+  formData.append("name", data?.name);
 
-		if (!response.ok) throw new Error(responseData.message);
+  const requestOptions = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    mode: "cors",
+    method: "PATCH",
+    redirect: "follow",
+    body: formData,
+  };
 
-		return responseData;
-	} catch (error) {
-		throw error;
-	}
+  try {
+    const response = await fetch(`${BASE_URL}categories/${id}`, requestOptions);
+    const responseData = await response.json();
+
+    if (!response.ok) throw new Error(responseData.message);
+
+    return responseData;
+  } catch (error) {
+    throw error;
+  }
 };
 
 // ========== Delete category ========== //
 
 export const deleteCategory = async (id) => {
-	const token = getToken();
+  const token = getToken();
 
-	var requestOptions = {
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-			Authorization: `Bearer ${token}`,
-		},
-		mode: "cors",
-		method: "DELETE",
-		redirect: "follow",
-	};
+  var requestOptions = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    mode: "cors",
+    method: "DELETE",
+    redirect: "follow",
+  };
 
-	try {
-		const response = await fetch(`${BASE_URL}categories/${id}`, requestOptions);
-		const data = await response.json();
+  try {
+    const response = await fetch(`${BASE_URL}categories/${id}`, requestOptions);
+    const data = await response.json();
 
-		if (!response.ok) throw new Error(data.message);
+    if (!response.ok) throw new Error(data.message);
 
-		return data;
-	} catch (error) {
-		throw error;
-	}
+    return data;
+  } catch (error) {
+    throw error;
+  }
 };
