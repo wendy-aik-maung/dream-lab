03db4096e12b90c9ecc0fa82b4fc039a5a9389b4
Wendy <111409717+wendy-aik-maung@users.noreply.github.com>
2023-03-18 17:54:02 +0630

    implment category features

--- src/pages/admin/category/DeleteModal.jsx
+++ src/pages/admin/category/DeleteModal.jsx
@@ -1,34 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { RiDeleteBin5Line } from "react-icons/ri";
+import { useDeleteCategory } from "../../../hooks/useCategory";
+import { useParams } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
+import ErrorMessage from "../../../components/form/ErrorMessage";
 
-const DeleteModal = ({ setDeleteStatus }) => {
-  return (
-    <div
-      className="fixed top-0 left-0 w-full h-full z-50 bg-black bg-opacity-80 flex justify-center items-center "
-      onClick={() => setDeleteStatus(false)}
-    >
-      <div
-        className="bg-white rounded-lg p-8 flex flex-col items-center w-[400px]"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <RiDeleteBin5Line size={96} fill={"#bfbfbf"} className="mb-8" />
-        <p className="font-medium text-xl text-center mb-12 text-[#333]">
-          Are you sure want to delete this category?
-        </p>
-        <div className="flex gap-4">
-          <button
-            className="font-medium bg-[#E4E4E4] px-4 py-2 rounded"
-            onClick={() => setDeleteStatus(false)}
-          >
-            Cancel
-          </button>
-          <button className="font-medium bg-[#BC3131] px-4 py-2 rounded text-white disabled:cursor-not-allowed disabled:bg-opacity-75">
-            Delete
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+const DeleteModal = ({ id, setDeleteStatus, handleRefreshData }) => {
+	const deleteCategoryMutation = useDeleteCategory();
+
+	const handleDelete = async () => {
+		deleteCategoryMutation.mutate(id);
+	};
+
+	useEffect(() => {
+		if (deleteCategoryMutation.isSuccess) {
+			handleRefreshData();
+			setDeleteStatus(false);
+		}
+	}, [deleteCategoryMutation.isSuccess]);
+
+	return (
+		<div
+			className="fixed top-0 left-0 w-full h-full z-50 bg-black bg-opacity-80 flex justify-center items-center "
+			onClick={() => setDeleteStatus(false)}>
+			<div
+				className="bg-white rounded-lg p-8 flex flex-col items-center w-[400px]"
+				onClick={(e) => e.stopPropagation()}>
+				<RiDeleteBin5Line size={96} fill={"#bfbfbf"} className="mb-8" />
+				<p className="font-medium text-xl text-center mb-12 text-[#333]">
+					Are you sure want to delete this category?
+				</p>
+				<div className="flex gap-7">
+					<button
+						className="font-medium bg-[#E4E4E4] px-4 py-2 rounded"
+						onClick={() => setDeleteStatus(false)}>
+						Cancel
+					</button>
+					{deleteCategoryMutation.isError && (
+						<ErrorMessage message={deleteCategoryMutation.error.message} />
+					)}
+					<button
+						onClick={handleDelete}
+						className="font-medium bg-[#BC3131] px-4 py-2 rounded text-white disabled:cursor-not-allowed disabled:bg-opacity-75">
+						{deleteCategoryMutation.isLoading ? (
+							<div className="flex items-center justify-center gap-3">
+								<ClipLoader color="white" size={23} />
+								<span>Deleting...</span>
+							</div>
+						) : (
+							<span>Delete</span>
+						)}
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default DeleteModal;
--- src/pages/admin/category/index.jsx
+++ src/pages/admin/category/index.jsx
@@ -7,54 +7,68 @@ import DeleteModal from "./DeleteModal";
 import EditCategory from "./EditCategory";
 import { ClipLoader } from "react-spinners";
 const CategoryIndex = () => {
-  const [createStatus, setCreateStatus] = useState();
-  const [editStatus, setEditStatus] = useState(false);
-  const [deleteStatus, setDeleteStatus] = useState(false);
+	const [createStatus, setCreateStatus] = useState();
+	const [editStatus, setEditStatus] = useState(false);
+	const [deleteStatus, setDeleteStatus] = useState(false);
+	const [id, setId] = useState("");
 
-  const { isLoading, data: categories, refetch, isError } = useGetCategories();
+	const { isLoading, data: categories, refetch, isError } = useGetCategories();
 
-  const handleRefreshData = () => {
-    refetch();
-  };
+	const handleRefreshData = () => {
+		refetch();
+	};
 
-  if (isLoading) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <ClipLoader color="black" size={32} />
-      </div>
-    );
-  }
-  if (isError) {
-    return <p className="text-red-500 font-poppins">{error.message}</p>;
-  }
+	if (isLoading) {
+		return (
+			<div className="flex justify-center items-center h-screen">
+				<ClipLoader color="black" size={32} />
+			</div>
+		);
+	}
+	if (isError) {
+		return <p className="text-red-500 font-poppins">{error.message}</p>;
+	}
 
-  return (
-    <section>
-      <CreatePageTitle
-        title="Category"
-        buttonTitle="Create New"
-        setCreateStatus={setCreateStatus}
-      />
-      <section className="flex flex-col gap-4 mt-12">
-        {categories.map((category) => (
-          <CategoryItem
-            key={category.id}
-            category={category}
-            setEditStatus={setEditStatus}
-            setDeleteStatus={setDeleteStatus}
-          />
-        ))}
-      </section>
-      {createStatus ? (
-        <CreateCategory
-          setCreateStatus={setCreateStatus}
-          handleRefreshData={handleRefreshData}
-        />
-      ) : null}
-      {editStatus ? <EditCategory setEditStatus={setEditStatus} /> : null}
-      {deleteStatus ? <DeleteModal setDeleteStatus={setDeleteStatus} /> : null}
-    </section>
-  );
+	return (
+		<section>
+			<CreatePageTitle
+				title="Category"
+				buttonTitle="Create New"
+				setCreateStatus={setCreateStatus}
+			/>
+			<section className="flex flex-col gap-4 mt-12">
+				{categories.map((category) => (
+					<CategoryItem
+						key={category.id}
+						setId={setId}
+						category={category}
+						setEditStatus={setEditStatus}
+						setDeleteStatus={setDeleteStatus}
+					/>
+				))}
+			</section>
+			{createStatus ? (
+				<CreateCategory
+					setCreateStatus={setCreateStatus}
+					handleRefreshData={handleRefreshData}
+				/>
+			) : null}
+			{editStatus ? (
+				<EditCategory
+					id={id}
+					setEditStatus={setEditStatus}
+					handleRefreshData={handleRefreshData}
+				/>
+			) : null}
+			{deleteStatus ? (
+				<DeleteModal
+					id={id}
+					setDeleteStatus={setDeleteStatus}
+					handleRefreshData={handleRefreshData}
+				/>
+			) : null}
+		</section>
+	);
 };
 
 export default CategoryIndex;
