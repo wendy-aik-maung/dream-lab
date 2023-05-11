378ac32df9dfce175c5a681500a59ac37f406509
aungchanmyaethaw <aungchanmyaethaw0610@gmail.com>
2023-05-11 08:14:38 +0630

    admin username changed

--- src/hooks/useAdminLogin.js
+++ src/hooks/useAdminLogin.js
@@ -3,6 +3,7 @@ import { useMutation } from "@tanstack/react-query";
 import {
   TOKEN_LOCAL_STORAGE,
   USER_DATA_LOCAL_STORAGE,
+  useUserDataContext,
 } from "../contexts/UserDataContext";
 import { useNavigate } from "react-router-dom";
 import {
@@ -12,6 +13,7 @@ import {
 export const useAdminLogin = () => {
   const navigate = useNavigate();
   const { setIsAdmin } = useAdminAuthContext();
+  const { setUserData } = useUserDataContext();
   return useMutation({
     mutationFn: userLogin,
     onSuccess: (data) => {
@@ -19,6 +21,7 @@ export const useAdminLogin = () => {
       localStorage.setItem(TOKEN_LOCAL_STORAGE, data.access_token);
       localStorage.setItem(ISADMIN_LOCAL_STORAGE, JSON.stringify(true));
       setIsAdmin(true);
+      setUserData(data);
       navigate("/admin");
     },
   });
